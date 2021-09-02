ae8663de9e8320de6f096405cb7e05fb88c9be58
Hubert Pietrusiak <hubert.pietrusiak@gmail.com>
2021-09-02 13:13:25 +0200

    Add product-specific language file exporting
    Change-Id: I00aa7faeb2fce0dc7073b42ceaf77cf9bedd17b3

--- exporter.js
+++ exporter.js
@@ -3,6 +3,25 @@ const fs = require('fs-promise');
 const fsOriginal = require('fs');
 const path = require('path');
 
+const createDirs = outputPath => {
+  if (!fsOriginal.existsSync(outputPath)){
+    const parts = path.join(outputPath).split('/');
+    let dirpath = '';
+
+    parts.forEach((value, key) => {
+      if(value === '' || value.includes('.json')) {
+        return;
+      }
+
+      dirpath += value;
+      if(!fsOriginal.existsSync(dirpath)) {
+        fsOriginal.mkdirSync(dirpath);
+      }
+      dirpath += '/';
+    });
+  }
+}
+
 module.exports = (content, config) => {
   console.log('Exporting....');
 
@@ -11,24 +30,22 @@ module.exports = (content, config) => {
   config.languages.forEach(({columnName: lang, filename}) => {
     const outputPath = path.join(config.outputFolder, filename);
 
-    if (!fsOriginal.existsSync(config.outputFolder)){
-      const parts = path.join(config.outputFolder).split('/');
-      let dirpath = '';
-
-      parts.forEach((value, key) => {
-        if(value === '') {
-          return;
-        }
-
-        dirpath += value;
-        fsOriginal.mkdirSync(dirpath);
-        dirpath += '/';
-      });
+    createDirs(outputPath)
+    if(config.products && config.products.separatedOutputDirs) {
+      Object.values(config.products.separatedOutputDirs).forEach(value => {
+        createDirs(path.join(outputPath, value))
+      })
     }
 
-    console.log(content[lang]);
+    // console.log(content);
 
-    promises.push(fs.writeFile(outputPath,  JSON.stringify(content[lang], null, 2), {flag: 'w'}));
+    if(config.products && config.products.separatedOutputDirs) {
+      Object.keys(config.products.separatedOutputDirs).forEach(key => {
+        promises.push(fs.writeFile(path.join(config.outputFolder, config.products.separatedOutputDirs[key], filename),  JSON.stringify(content[lang][key], null, 2), {flag: 'w'}));
+      })
+    } else {
+      promises.push(fs.writeFile(outputPath,  JSON.stringify(content[lang], null, 2), {flag: 'w'}));
+    }
   });
 
   console.log('Exporting successful');
--- parser.js
+++ parser.js
@@ -1,12 +1,13 @@
 module.exports = (content, config) => {
   console.log('Parsing....');
   let result = {};
+  let productColumnName = 'Product'
 
   config.languages.forEach((lang) => {
     result[lang.columnName] = {};
 
-
     if(config.products) {
+      productColumnName = config.products.productColumnName || productColumnName;
       for (let prop in config.products.valueMap) {
         result[lang.columnName][config.products.valueMap[prop]] = {};
       }
@@ -15,10 +16,16 @@ module.exports = (content, config) => {
 
   content.forEach((row) => {
     config.languages.forEach((lang) => {
-      if(row[lang.columnName] || (!row[lang.columnName] && config.includeEmptyValues)) {
-        const value = row[lang.columnName] || '';
+      if(row[lang.columnName] || (!row[lang.columnName] && (config.includeEmptyValues || config.fallbackToLanguage))) {
+        let value = row[lang.columnName];
+        if(config.fallbackToLanguage) {
+          value = value || row[config.fallbackToLanguage]
+        }
+        if(config.includeEmptyValues) {
+          value = value || ''
+        }
         if(config.products) {
-          result[lang.columnName][config.products.valueMap[row.product]][row[config.keysColumnName]] = value;
+          result[lang.columnName][config.products.valueMap[row[productColumnName]]][row[config.keysColumnName]] = value;
         } else {
           result[lang.columnName][row[config.keysColumnName]] = value;
         }
@@ -26,6 +33,21 @@ module.exports = (content, config) => {
     });
 
   });
+
+  if(config.products && config.products.fallbackToProduct) {
+    config.languages.forEach(({columnName: lang}) => {
+      const productsToFallback = Object.values(config.products.valueMap).filter(product => product !== config.products.fallbackToProduct)
+      const fallbackProduct =  config.products.fallbackToProduct
+
+      productsToFallback.forEach(product => {
+        Object.keys(result[lang][fallbackProduct]).forEach(translationKey => {
+          if(!result[lang][product][translationKey]) {
+            result[lang][product][translationKey] = result[lang][fallbackProduct][translationKey]
+          }
+        })
+      })
+    })
+  }
 
   console.log('Parsing successful');
 
